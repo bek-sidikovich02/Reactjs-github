726ebf238f689d475f9855a75401e5d139de4a1e
ITishnik <developerhojikabir2004@gmail.com>
2022-04-14 15:25:28 +0500

    h1 yozdim

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>salom react app!</h1>
+  
 
       <h1>salom angular!</h1>
+      <h1> salom react</h1>
 
      
 
